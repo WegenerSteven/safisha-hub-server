124544a6833c07c041ab0075b9213a4419af9a9a
WegenerSteven <wegenersteven580@gmail.com>
2025-07-01 16:09:53 +0300

    create resource controllers, services and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { ServiceProviderModule } from './service-provider/service-provider.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PaymentsModule } from './payments/payments.module';
+import { LocationsModule } from './locations/locations.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { EmailServiceModule } from './email-service/email-service.module';
+import { SmsModule } from './sms/sms.module';
+import { AuthModule } from './auth/auth.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     DatabaseModule,
+    UsersModule,
+    ServicesModule,
+    ServiceProviderModule,
+    BookingsModule,
+    PaymentsModule,
+    LocationsModule,
+    ReviewsModule,
+    NotificationsModule,
+    EmailServiceModule,
+    SmsModule,
+    AuthModule,
+    AnalyticsModule,
   ],
   controllers: [],
   providers: [],
