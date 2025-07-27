cfa5e37f3528b43826a117a3c2f9288a828401fb
WegenerSteven <wegenersteven580@gmail.com>
2025-07-27 23:58:32 +0300

    implement payment feature

--- src/payment/payments.controller.ts
+++ src/payment/payments.controller.ts
@@ -1,13 +1,29 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Get, UseGuards } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
 import { PaystackService } from './paystack.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from '../auth/decorators/public.decorators';
+import { AtGuard } from 'src/auth/guards';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { GetCurrentUserId } from 'src/auth/decorators/get-current-user.decorator';
+import { PaymentStatus } from './entities/payment.entity';
 
 @ApiTags('payments')
 @Public()
 @Controller('payments')
 export class PaymentsController {
-  constructor(private readonly paystackService: PaystackService) {}
+  constructor(
+    private readonly paystackService: PaystackService,
+    private readonly paymentsService: PaymentsService,
+  ) {}
+  @UseGuards(AtGuard, RolesGuard)
+  @Get('my')
+  @ApiOperation({ summary: 'Get payments for current user' })
+  @ApiResponse({ status: 200, description: 'List of payments' })
+  async getMyPayments(@GetCurrentUserId() userId: string) {
+    return this.paymentsService.findByUserId(userId);
+  }
 
   @Post('initialize')
   @ApiOperation({ summary: 'Initialize payment' })
@@ -73,9 +89,32 @@ export class PaymentsController {
   }
 
   @Post('verify')
+  @UseGuards(AtGuard, RolesGuard)
   @ApiOperation({ summary: 'Verify payment status' })
   @ApiResponse({ status: 200, description: 'Payment verified' })
-  async verifyPayment(@Body() body: { reference: string }) {
-    return this.paystackService.verifyPayment(body.reference);
+  async verifyPayment(
+    @Body() body: { reference: string; bookingId?: string; amount: number },
+    @GetCurrentUserId() userId: string,
+  ) {
+    const result = await this.paystackService.verifyPayment(body.reference);
+    if (
+      typeof result === 'object' &&
+      result !== null &&
+      'status' in result &&
+      result.status === true
+    ) {
+      if (!body.bookingId) {
+        throw new Error('Missing bookingId in payment verification request');
+      }
+      await this.paymentsService.create({
+        reference: body.reference,
+        booking_id: body.bookingId,
+        amount: body.amount,
+        user_id: userId,
+        status: PaymentStatus.SUCCEEDED as PaymentStatus,
+        created_at: new Date(),
+      });
+    }
+    return result;
   }
 }
